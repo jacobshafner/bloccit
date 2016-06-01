f759fc9dc11f43946c05695543a2985757e0a7d1
Jacob Hafner <jhafner1990@gmail.com>
2016-05-31 23:22:02 -0700

    checkpoint 35 complete

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -5,6 +5,8 @@ RSpec.describe Comment, type: :model do
 	let(:post) { topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph) }
 	let(:comment) { Comment.create!(body: 'Comment Body', post: post) }
 
+
+
 	describe "attributes" do
 		it "has a body attribute" do
 			expect(comment).to have_attributes(body: "Comment Body")
