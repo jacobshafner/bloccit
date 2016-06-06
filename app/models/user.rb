6faf01385b652941862a51c984486c90506d7fbf
Jacob Hafner <jhafner1990@gmail.com>
2016-06-06 12:08:48 -0700

    completed checkpoint 37

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ActiveRecord::Base
 
   before_save { self.email = email.downcase if email.present? }
-  before_save :format_name
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
   validates :password, presence: true, length: { minimum: 6 }, unless: :password_digest
   validates :password, length: { minimum: 6 }, allow_blank: true
